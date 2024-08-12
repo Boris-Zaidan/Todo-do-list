57e2c8bc6c52e0f6c994fe80b8e962a98df4bb00
Boris zaidan <Bzmgs31@gmail.com>
2024-08-12 12:54:27 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,8 @@
 // seleção de elementos
+const todoForm = document.querySelector("todo-form");
+const todoInput = document.querySelector("todo-Input");
+const todoList = document.querySelector("todo-list");
+const editForm = document.querySelector("edit-form");
+const editInput = document.querySelector("edit-input");
+const cancelEditBtn = document.querySelector("cancel-edit-btn");
 
-//funções
-
-//Eventos
